4c7f34ec06d88bfe19cada9cdd1157baf8cd8c62
Robin <lxs5171@mavs.uta.edu>
2021-11-15 21:32:59 -0600

    version: 1.41.111.45.42.26-2

--- aces.sh
+++ aces.sh
@@ -20,7 +20,7 @@ deployment_config_directory='ethernet-16'
 
 if [[ "$cmd" != "reuse" && "$cmd" != "reboot-all" && "$cmd" != "reboot-cluster" && "$cmd" != "stop" && "$cmd" != "addon" ]];then
     # export version to modules
-    for f in jade-go jade-ui/src jade-devops jade-devops jadesdk plankton jade-tests/sim-v2 jade-app-temp-hum; do
+    for f in jade-go jade-ui/src jade-devops jade-devops jadesdk plankton jade-tests/sim-v3 jade-app-temp-hum; do
         echo '{ "version": "'${version}'" }' > ${f}/version.json
     done
 
@@ -83,7 +83,7 @@ if [[ "$cmd" != "reuse" && "$cmd" != "reboot-all" && "$cmd" != "reboot-cluster" 
     echo "packing source code"
 
     cp -r jade-ui/build jade-go/ui 
-    tar czf jadelet.source.tar.gz jade-go jadesdk plankton jade-devops jade-tests/sim-v2 jade-app-temp-hum
+    tar czf jadelet.source.tar.gz jade-go jadesdk plankton jade-devops jade-tests/sim-v3 jade-app-temp-hum
 
     if [[ "$cmd" != "devops" ]];then
         cmd="build-and-push"
