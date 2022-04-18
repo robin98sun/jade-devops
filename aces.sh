ccc075c4bbc089ab5c825c8e01bc3df12415357b
Robin <lxs5171@mavs.uta.edu>
2022-04-18 17:31:59 -0500

    version: 2.0.0.0.0.0.0-alpha

--- aces.sh
+++ aces.sh
@@ -9,11 +9,12 @@ password=$4
 scheme=$5
 
 if [[ "$scheme" == "" ]];then
-    scheme="scheme-1tier-ethernet32"
+    # scheme="scheme-1tier-ethernet32"
     # scheme="scheme-1tier-ethernet16"
     # scheme="scheme-multi_tiers-ethernet16"
     # scheme="scheme-2tiers-ethernet16"
     # scheme="scheme-2tiers-ethernet-partial"
+    scheme="scheme-2tiers-4clusters-32pi-decentralized"
 fi
 scheme_deployment_cmd="./jade-devops/deployments/lab/schemes/${scheme}/deployment.sh"
 
