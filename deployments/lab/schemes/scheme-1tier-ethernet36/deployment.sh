bb04248af9dc6c1d75b6ab0ee1fc3c882ac287b3
Robin <lxs5171@mavs.uta.edu>
2022-07-17 19:57:01 -0500

    2.6.68.0.7.1.19-0

---
+++ deployments/lab/schemes/scheme-1tier-ethernet36/deployment.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+version=$1
+if [[ "$version" == "" ]]; then
+    echo "VERSION number is missing when deploying JADE clusters"
+    exit 1
+fi
+
+deployment_config_directory=$2
+if [[ "$deployment_config_directory" == "" ]]; then
+    deployment_config_directory='ethernet-16'
+fi
+
+env_dir=./jade-devops/deployments/lab/env
+rm -rf ${env_dir}
+mkdir -p ${env_dir}
+
+./jade-devops/speed-deploy-by-config.py \
+    --master ./jade-devops/deployments/lab/hosts/$deployment_config_directory/cluster-nodes-01.json \
+    --agents ./jade-devops/deployments/lab/hosts/$deployment_config_directory/master-node.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/cluster-nodes-02.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/cluster-nodes-03.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/cluster-nodes-04.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-1.1.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-1.2.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-1.3.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-1.4.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.1.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.2.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.3.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.4.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-3.1.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-3.2.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-3.3.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-3.4.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.1.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.2.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.3.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.4.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-5.0.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-5.1.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-5.2.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-5.3.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-5.4.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-5.5.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-5.6.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-5.7.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-6.0.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-6.1.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-6.2.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-6.3.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-6.4.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-6.5.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-6.6.json \
+             ./jade-devops/deployments/lab/hosts/$deployment_config_directory/agent-nodes-6.7.json \
+    --env-dir ${env_dir} \
+    --version ${version}
