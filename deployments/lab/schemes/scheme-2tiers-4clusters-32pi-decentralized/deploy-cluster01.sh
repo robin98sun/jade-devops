72acd3b571c9e14d3d414b506ddea6b4de20fa03
Robin <lxs5171@mavs.uta.edu>
2022-09-17 02:54:17 -0500

    2.6.131.0.12.2.44-0

--- deployments/lab/schemes/scheme-2tiers-4clusters-32pi-decentralized/deploy-cluster01.sh
+++ deployments/lab/schemes/scheme-2tiers-4clusters-32pi-decentralized/deploy-cluster01.sh
@@ -34,17 +34,24 @@ fi
 
 # tier1
 # cluster1: cluster 01: SEIR225
+# ${root_dir}/speed-deploy-by-config.py \
+#     --registry ${root_dir}/deployments/lab/hosts/$deployment_config_directory/master-node.json \
+#     --master ${root_dir}/deployments/lab/hosts/$deployment_config_directory/cluster-nodes-01.json \
+#     --agents ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.1.json \
+#              ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.2.json \
+#              ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.3.json \
+#              ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.4.json \
+#              ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.1.json \
+#              ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.2.json \
+#              ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.3.json \
+#              ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.4.json \
+#     --env-dir ${env_dir} \
+#     --version ${version} \
+#     --partial-deployment 'all' $additional_args
+
 ${root_dir}/speed-deploy-by-config.py \
     --registry ${root_dir}/deployments/lab/hosts/$deployment_config_directory/master-node.json \
     --master ${root_dir}/deployments/lab/hosts/$deployment_config_directory/cluster-nodes-01.json \
-    --agents ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.1.json \
-             ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.2.json \
-             ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.3.json \
-             ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-2.4.json \
-             ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.1.json \
-             ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.2.json \
-             ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.3.json \
-             ${root_dir}/deployments/lab/hosts/$deployment_config_directory/agent-nodes-4.4.json \
     --env-dir ${env_dir} \
     --version ${version} \
     --partial-deployment 'all' $additional_args
