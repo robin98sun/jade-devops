5c1e5f704571d954c9e881af28bc5b20535f68ed
Robin <lxs5171@mavs.uta.edu>
2023-02-13 02:07:14 -0600

    3-autoscaling-1.3.4-4

--- addons/jade-addon-http-server.py
+++ addons/jade-addon-http-server.py
@@ -36,32 +36,44 @@ from resource_manager.cpu_resource import get_kube_all_pods_cgroup_cpu_info
 from resource_manager.cpu_resource import get_kube_pod_cgroup_cpu_resource
 from resource_manager.cpu_resource import update_kube_pod_cgroup_cpu_resource
 
+def jadify_response(res, err = None):
+    if err is not None:
+        return {
+            "error": err,
+        }
+
+    return {
+        "status": "OK",
+        "payload": res,
+    }
+
+
 @app.route("/cpu-cores", methods=["GET"])
 def api_cpu_cores():
-    return jsonify(get_cpu_cores())
+    return jsonify(jadify_response(get_cpu_cores()))
 print("the controller for [GET]/cpu-cores is registered")
 
 
 @app.route("/kube-overall-cpu-shares", methods=["GET"])
 def api_kube_cpu_shares():
-    return jsonify(get_kube_overall_cpu_shares())
+    return jsonify(jadify_response(get_kube_overall_cpu_shares()))
 print("the controller for [GET]/kube-ovall-cpu-shares is registered")
 
 
 @app.route("/kube-besteffort-pods-cpu-resources", methods=["GET"])
 def api_get_kube_besteffort_pods_cpu_resources():
-    return jsonify(get_kube_pods_cgroup_cpu_info(is_besteffort = True))
+    return jsonify(jadify_response(get_kube_pods_cgroup_cpu_info(is_besteffort = True)))
 print("the controller for [GET]/kube-besteffort-pods-cpu-resources is registered")
 
 
 @app.route("/kube-fixed-pods-cpu-resources", methods=["GET"])
 def api_get_kube_fixed_pods_cpu_resources():
-    return jsonify(get_kube_pods_cgroup_cpu_info(is_besteffort = False))
+    return jsonify(jadify_response(get_kube_pods_cgroup_cpu_info(is_besteffort = False)))
 print("the controller for [GET]/kube-fixed-pods-cpu-resources is registered")
 
 @app.route("/kube-all-pods-cpu-resources", methods=["GET"])
 def api_get_kube_all_pods_cgroup_cpu_info():
-    return jsonify(get_kube_all_pods_cgroup_cpu_info())
+    return jsonify(jadify_response(get_kube_all_pods_cgroup_cpu_info()))
 print("the controller for [GET]/kube-all-pods-cpu-resources is registered")
 
 
@@ -72,29 +84,29 @@ def api_get_kube_pod_cpu_quota():
     pod_uid = request.args.get('uid')
     is_besteffort_str = request.args.get('is_besteffort')
     if resource_type is None or resource_type == "" or pod_uid is None or pod_uid == "" or is_besteffort_str is None:
-        return "invalid request"
+        return jsonify(jadify_response(None, err="invalid request"))
 
     is_besteffort = False
     if is_besteffort_str == "" or is_besteffort_str.lower() == "true" or is_besteffort_str.lower() == "yes" or is_besteffort_str.lower() == "y" or is_besteffort_str.lower() == "t":
         is_besteffort = True
-    return jsonify(get_kube_pod_cgroup_cpu_resource(resource_type, pod_uid, is_besteffort))
+    return jsonify(jadify_response(get_kube_pod_cgroup_cpu_resource(resource_type, pod_uid, is_besteffort)))
 print("the controller for [GET]/kube-pod-cpu-resource is registered")
 
 @app.route("/kube-pod-cpu-resource", methods=["PUT"])
 def api_update_kube_pod_cpu_quota():
     req = request.get_json()
     if req is None or "uid" not in req or "value" not in req or "is_besteffort" not in req or "type" not in req:
-        return "invalid request"
+        return jsonify(jadify_response(None, err="invalid request"))
     passwd = None
     if "passwd" in req:
         passwd = req["passwd"]
-    return jsonify(update_kube_pod_cgroup_cpu_resource(
+    return jsonify(jadify_response(update_kube_pod_cgroup_cpu_resource(
         resource_type = req["type"],
         pod_uid = req["uid"],
         value = req["value"],
         passwd = passwd,
         is_besteffort = req["is_besteffort"],
-    ))
+    )))
 print("the controller for [PUT]/kube-pod-cpu-resource is registered")
 
 
